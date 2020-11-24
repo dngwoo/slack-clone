72eb3d12aa6c8b0e7c8911fc6d27b224d27f80a1
dngwoo <akfmzk84@naver.com>
2020-11-24 11:04:47 +0900

    db.ts - api 데이터 인터페이스 추가

---
+++ lecture/typings/db.ts
@@ -0,0 +1,46 @@
+// * 백엔드 개발자가 남겨준 api 데이터의 인터페이스.
+// * 보통 백엔드 개발자가 제공해준다.
+export interface IUser {
+  id: number;
+  nickname: string;
+  email: string;
+  Workspaces: IWorkspace[];
+}
+
+export interface IUserWithOnline extends IUser {
+  online: boolean;
+}
+
+export interface IChannel {
+  id: number;
+  name: string;
+  private: boolean;
+  WorkspaceId: number;
+}
+
+export interface IChat {
+  id: number;
+  UserId: number;
+  User: IUser;
+  content: string;
+  createdAt: Date;
+  ChannelId: number;
+  Channel: IChannel;
+}
+
+export interface IDM {
+  id: number;
+  SenderId: number;
+  Sender: IUser;
+  ReceiverId: number;
+  Receiver: IUser;
+  content: string;
+  createdAt: Date;
+}
+
+export interface IWorkspace {
+  id: number;
+  name: string;
+  url: string;
+  OwnerId: number;
+}
