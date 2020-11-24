6738a720f0949b20fcdc2a1080efd7cd168cbd63
dngwoo <akfmzk84@naver.com>
2020-11-24 11:58:44 +0900

    gravatar 적용

--- lecture/layouts/Workspace/index.tsx
+++ lecture/layouts/Workspace/index.tsx
@@ -13,6 +13,7 @@ import {
   WorkspaceWrapper,
 } from '@layouts/Workspace/styles';
 import useSWR from 'swr';
+import gravatar from 'gravatar';
 
 const Workspace = () => (
   const {data: userData} = useSWR('/api/user');
@@ -24,7 +25,8 @@ const Workspace = () => (
       { userData && (
         <RightMenu>
           <span>
-            <ProfileImg src={}></ProfileImg>
+            {/* 유저 이메일에 따라 알아서 아이콘 생성을 해줌. s = size, retro = 깃헙 같은 아이콘 */}
+            <ProfileImg src={gravatar.url(userData.email, {s: '36px', d: 'retro'})}></ProfileImg>
           </span>
         </RightMenu>
       )}
